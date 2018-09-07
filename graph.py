1b32927e0b4d6443a1cb41ac779dabdb11a9c4aa
Marcos Magno <marcosf40@hotmail.com>
2018-09-07 16:52:49 -0300

    transmissao de video

--- graph.py
+++ graph.py
@@ -2,8 +2,7 @@
 Demo of the errorbar function
 """
 # -*- coding: utf-8 -*-
-
-from pylab import*
+from pylab import *
 import numpy
 import matplotlib.pyplot as plt
 
@@ -11,8 +10,8 @@ import matplotlib.pyplot as plt
 # Dual
 x = (0,1)
 #	 Single 	 Dual
-y = [129.748485, 221,151515 ]  # media
-yerr = [9.851692, 11,779749]  # desvio padrao
+y = [1.818988, 1.563357 ]  # media
+yerr = [0.844069, 0.814921]  # desvio padrao
 width = 0.15 
 
 fig = plt.figure()
@@ -21,16 +20,16 @@ fig = plt.figure()
 
 #plt.xticks(np.arange(min(x), max(x)+1, 1.0), x)
 # standard error bars
-bar1 = plt.bar(x[0], y[0], width=0.60,color='r',  label='Single BW', yerr=yerr[0])
+bar1 = plt.bar(x[0], y[0], width=0.60,color='r',  label='Um celular', yerr=yerr[0])
 
-bar2 = plt.bar(x[1], y[1], width=0.60,  color='b',  label='Wider BW',  yerr=yerr[1], hatch="/", edgecolor='black')
+bar2 = plt.bar(x[1], y[1], width=0.60,  color='b',  label='Dois celulares',  yerr=yerr[1], hatch="/", edgecolor='black')
 #plt.yticks(labelsize=40)
 plt.ylabel(r"$y$",fontsize=12)
 plt.xlabel(r"$y$",fontsize=12)
 
 plt.xlabel('Node ID')
-plt.ylabel('Packets received (pkts/s)')
-plt.xticks(x, ('Sink Node', 'Sink Node'))
+plt.ylabel('Bitrate Mb/s')
+plt.xticks(x, ('Um celular', 'Dois celulares'))
 plt.legend()
 
 plt.show()
